a3423cf5ee8c34280a278f0c9708b8177c381e8d
Arturo Parra <arturoparravalencia@gmail.com>
2024-09-02 21:14:14 -0500

    update a controller

--- crypto_exchange_mvc/src/main/java/org/example/controller/TransactionController.java
+++ crypto_exchange_mvc/src/main/java/org/example/controller/TransactionController.java
@@ -3,13 +3,15 @@ package org.example.controller;
 import org.example.model.User;
 
 public class TransactionController {
+    private ConsoleView view;
     private User user;
 
-    public TransactionController(User user){
+    public TransactionController(ConsoleView view,User user){
+        this.view = view;
         this.user = user;
     }
 
-    public displayTransactions(){
-        user.getTransactions().toString();
+    public String displayTransactions(){
+        return user.getTransactions().toString();
     }
 }
